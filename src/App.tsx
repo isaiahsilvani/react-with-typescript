8a0dbe5624600a67a5bfbde846a03c21df2fbc0c
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-29 13:21:26 -0400

    hook change number

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState(5)
+
+  const changeNumber = () => {
+    setNumber(10)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <button onClick={changeNumber}>Change</button>
         </p>
         <a
           className="App-link"
